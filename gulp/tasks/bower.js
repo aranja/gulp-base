854a2d25788d2ff53160acc55cf59b4837bb4f45
Eirikur Nilsson <eirikur@nilsson.is>
2014-07-12 17:48:56 +0000

    Switch to non-deprecated main-bower-files

--- gulp/tasks/bower.js
+++ gulp/tasks/bower.js
@@ -1,7 +1,7 @@
 module.exports = function(gulp) {
   var notify = require("gulp-notify");
   var bower = require('bower');
-  var bowerFiles = require('gulp-bower-files');
+  var bowerFiles = require('main-bower-files');
   var filter = require('gulp-filter');
   var rename = require('gulp-rename');
 
@@ -14,7 +14,7 @@ module.exports = function(gulp) {
         var jsFilter = filter(['**/*.js', '**/*.map']),
           lessFilter = filter('**/*.less');
 
-        return bowerFiles()
+        return gulp.src(bowerFiles())
           .pipe(rename({dirname: ''}))
           .pipe(jsFilter)
           .pipe(gulp.dest(gulp.config.source + '/js/vendor/'))
