df8a352c5f09fa5ed6d8bd1ab344c19a7c45287e
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2014-07-24 16:05:59 +0000

    Use gutil.env because gulp.env has been deprecated

--- gulp/index.js
+++ gulp/index.js
@@ -3,7 +3,7 @@ var gutil = require('gulp-util');
 var glob = require('glob');
 var tasks = glob.sync('*.js', {cwd: './gulp/tasks/'});
 
-gulp.config = require(gulp.env.prod ? './config.prod.json' : './config.json');
+gulp.config = require(gutil.env.prod ? './config.prod.json' : './config.json');
 
 tasks.forEach(function(task) {
   require('./tasks/' + task)(gulp, gutil);
