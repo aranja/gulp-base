4e5e680b9e676ec53f9906c3ec0f9839f9734ddc
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2015-01-11 19:23:15 +0000

    Fix #7 - images task wasn't being run

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,10 +4,11 @@ module.exports = function(gulp) {
     gulp.watch(gulp.config.source + '/js/**/*.js', ['browserify']);
     gulp.watch([
         gulp.config.source + '/*.html',
-        gulp.config.source + '/img/**/*.ico',
+        gulp.config.source + '/img/**/*.{png,gif,jpg,jpeg,svg}',
         gulp.config.source + '/fonts/**/*',
         gulp.config.source + '/videos/**/*'
     ], ['copy']);
     gulp.watch(gulp.config.source + '/views/**/*.jade', ['jade']);
+    gulp.watch(gulp.config.source + '/img/**/*.{png,gif,jpg,jpeg,svg}', ['images']);
   });
 };
