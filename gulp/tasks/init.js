5f00a186ed788aeeac12a47559460c8a0071b187
David Blurton <david.blurton@gmail.com>
2015-01-11 20:45:05 +0000

    Remove bower

--- gulp/tasks/init.js
+++ gulp/tasks/init.js
@@ -2,6 +2,6 @@ var runSequence = require('run-sequence');
 
 module.exports = function(gulp) {
   gulp.task('init', function(cb) {
-    runSequence('bower', 'build', 'server', cb);
+    runSequence('build', 'server', cb);
   });
 };
