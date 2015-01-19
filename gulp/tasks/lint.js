3f3638db53e04f00637309ed8a30952c5bd80d9a
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2015-01-19 22:38:33 +0000

    Add jscs style checker

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -2,13 +2,23 @@
 
 module.exports = function(gulp) {
   var eslint = require('gulp-eslint');
+  var jscs = require('gulp-jscs');
 
-  gulp.task('lint', function() {
-    return gulp.src([
-      gulp.config.source + '/js/**/*.js',
-      './gulp/**/*.js'
-    ])
+  var files = [
+    gulp.config.source + '/js/**/*.js',
+    './gulp/**/*.js'
+  ];
+
+  gulp.task('jscs', function() {
+    return gulp.src(files)
+      .pipe(jscs());
+  });
+
+  gulp.task('eslint', function() {
+    return gulp.src(files)
       .pipe(eslint())
       .pipe(eslint.format());
   });
+
+  gulp.task('lint', ['jscs', 'eslint']);
 };
