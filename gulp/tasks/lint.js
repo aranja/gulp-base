c73e070c77338b95da663fae6d3b5f2fb539bbf7
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2015-01-19 22:19:56 +0000

    Lint task applies also to gulp tasks

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,8 +1,13 @@
+'use strict';
+
 module.exports = function(gulp) {
   var eslint = require('gulp-eslint');
 
   gulp.task('lint', function() {
-    return gulp.src([gulp.config.source + '/js/**/*.js'])
+    return gulp.src([
+      gulp.config.source + '/js/**/*.js',
+      './gulp/**/*.js'
+    ])
       .pipe(eslint())
       .pipe(eslint.format());
   });
