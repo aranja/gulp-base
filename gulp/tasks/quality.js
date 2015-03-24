4d131b9dbb5c2462499985d3b6f610480bf34243
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2015-03-24 19:34:51 +0000

    Abstract files

--- gulp/tasks/quality.js
+++ gulp/tasks/quality.js
@@ -1,11 +1,13 @@
 var jshint = require('gulp-jshint');
 
 module.exports = function(gulp) {
+  var files = [
+    gulp.config.source + '/js/**/*.js',
+    './gulp/tasks/*.js'
+  ];
+
   gulp.task('code-quality', function() {
-    return gulp.srcWithErrorHandling([
-      gulp.config.source + '/js/**/*.js',
-      './gulp/tasks/*.js'
-    ])
+    return gulp.srcWithErrorHandling(files)
       .pipe(jshint())
       .pipe(jshint.reporter('jshint-stylish'));
   });
