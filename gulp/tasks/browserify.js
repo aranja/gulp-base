b039895c7f0b4a315cd93c5c6a772f61022909b3
Ægir Giraldo Þorsteinsson <aegir@thorsteinsson.is>
2015-04-20 10:06:28 +0000

    Fix uglify for prod

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,6 +1,7 @@
 var browserify = require('browserify');
 var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
 module.exports = function(gulp, gutil) {
   var prod = gutil.env.prod;
@@ -11,9 +12,10 @@ module.exports = function(gulp, gutil) {
         debug: !prod
       })
       .bundle()
-      .on('error', gulp.errorHandler)
       .pipe(source('app.js'))
+      .pipe(buffer())
       .pipe(!prod ? gutil.noop() : uglify({preserveComments: 'some'}))
+      .on('error', gulp.errorHandler)
       .pipe(gulp.dest(gulp.config.target + '/js/'));
   });
 };
