d37cc0bd3565645185fca3eb23fb6f78efcf6d57
Ægir Giraldo Þorsteinsson <aegir@thorsteinsson.is>
2015-04-21 00:56:37 +0000

    Browser-sync and open now a part of server

---
+++ gulp/tasks/server.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var gopen = require('gulp-open');
+var browserSync = require('browser-sync').create();
+var config = require('../config');
+
+gulp.task('server-browsersync', ['watch'], function() {
+  browserSync.init({
+    server: {
+      baseDir: config.target
+    }
+  });
+  gulp.watch(config.target + '/**/*').on('change', browserSync.reload);
+});
+
+gulp.task('server', ['server-browsersync'] function() {
+  gulp.src(config.target + '/index.html').pipe(gopen('', {
+    url: 'http://localhost:' + config.port,
+    app: 'Google Chrome'
+  }));
+});
