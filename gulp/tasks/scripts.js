f314e974b30ef495600e78c42759ac83c44177d0
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2015-10-10 12:21:11 +0000

    Add source map

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -4,6 +4,7 @@ import browserify from 'browserify';
 import babelify from 'babelify';
 import minify from 'gulp-uglify';
 import source from 'vinyl-source-stream';
+import sourceMaps from 'gulp-sourcemaps';
 import buffer from 'vinyl-buffer';
 import config from '../config';
 import errorHandler from '../utils/error-handler';
@@ -18,6 +19,8 @@ gulp.task('scripts', ['lint'], () => {
     .on('error', errorHandler)
     .pipe(source('app.js'))
     .pipe(buffer())
+    .pipe(sourceMaps.init({loadMaps: true}))
     .pipe(config.minify ? minify({preserveComments: 'some'}) : gutil.noop())
+    .pipe(sourceMaps.write('.'))
     .pipe(gulp.dest(`${config.target}/scripts/`));
 });
